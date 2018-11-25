7902e6d4a4bd9b46e228e05c3e79228cb50b672b
ggilligan12 <ggilligan12@gmail.com>
2018-11-25 18:33:40 +0000

    Added Mapbox dependencies to Gradle Script Added Fine Location, Internet and Network State permissions in manifest Added lifecycle function shells to MainActivity Added location finding and manipulation shells to main activity Implemented the DownloadCompleteRunner class Implemented the DownloadFileTask class

---
+++ app/src/main/java/com/example/george/coinz/DownloadCompleteRunner.java
@@ -0,0 +1,8 @@
+package com.example.george.coinz;
+
+public class DownloadCompleteRunner {
+
+    static String result;
+    public static void downloadComplete(String result) { DownloadCompleteRunner.result = result; }
+
+}
---
+++ app/src/main/java/com/example/george/coinz/MainActivity.java
@@ -0,0 +1,229 @@
+package com.example.george.coinz;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.mapbox.android.core.location.LocationEngine;
+import com.mapbox.android.core.location.LocationEngineListener;
+import com.mapbox.android.core.location.LocationEnginePriority;
+import com.mapbox.android.core.location.LocationEngineProvider;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.android.core.permissions.PermissionsManager;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode;
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationEngineListener, PermissionsListener {
+
+
+    private String tag = "MainActivity";
+    private MapView mapView;
+    private MapboxMap map;
+    private PermissionsManager permissionsManager;
+    private LocationEngine locationEngine;
+    private LocationLayerPlugin locationLayerPlugin;
+    private Location originLocation;
+    private String downloadDate = ""; // Format: YYYY/MM/DD
+    private final String preferencesFile = "MyPrefsFile"; // for storing preferences
+
+    // -------Lifecycle Functions-------- //
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        // Mapbox Access token
+        Mapbox.getInstance(getApplicationContext(), getString(R.string.mapbox_access_token));
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                if (mapboxMap == null) { Log.d(tag, "[onMapReady] mapBox is null"); }
+                else {
+                    map = mapboxMap;
+                    // Set user interface options
+                    map.getUiSettings().setCompassEnabled(true);
+                    map.getUiSettings().setZoomControlsEnabled(true);
+                    // Make location information available
+                    enableLocation();
+                }
+            }
+        });
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+
+        // Restore preferences
+        SharedPreferences settings = getSharedPreferences(preferencesFile, Context.MODE_PRIVATE);
+        // use ”” as the default value (this might be the first time the app is run)
+        downloadDate = settings.getString("lastDownloadDate", "");
+        Log.d(tag, "[onStart] Recalled lastDownloadDate is ’" + downloadDate + "’");
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+    /*@Override
+    protected void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }*/
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+    /*@Override
+    protected void onSaveInstanceState() {
+        super.onSaveInstanceState();
+        mapView.onSaveInstanceState();
+    }*/
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(tag, "[onStop] Storing lastDownloadDate of " + downloadDate);
+        // All objects are from android.context.Context
+        SharedPreferences settings = getSharedPreferences(preferencesFile, Context.MODE_PRIVATE);
+        // We need an Editor object to make preference changes.
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("lastDownloadDate", downloadDate);
+        // Apply the edits!
+        editor.apply();
+    }
+
+    // Auxiliary methods
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        if (mapboxMap == null) { Log.d(tag, "[onMapReady] mapBox is null"); }
+        else {
+            map = mapboxMap;
+            // Set user interface options
+            map.getUiSettings().setCompassEnabled(true);
+            map.getUiSettings().setZoomControlsEnabled(true);
+            // Make location information available
+            enableLocation();
+        }
+    }
+
+    private void enableLocation() {
+        if (PermissionsManager.areLocationPermissionsGranted(this)) {
+            Log.d(tag, "Permissions are granted");
+            initializeLocationEngine();
+            initializeLocationLayer();
+        }
+        else {
+            Log.d(tag, "Permissions are not granted");
+            permissionsManager = new PermissionsManager(this);
+            permissionsManager.requestLocationPermissions(this);
+        }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private void initializeLocationEngine() {
+        locationEngine = new LocationEngineProvider(this).obtainBestLocationEngineAvailable();
+        locationEngine.setInterval(5000); // preferably every 5 seconds
+        locationEngine.setFastestInterval(1000); // at most every second
+        locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY);
+        locationEngine.activate();
+        Location lastLocation = locationEngine.getLastLocation();
+        if (lastLocation != null) { originLocation = lastLocation; setCameraPosition(lastLocation); }
+        else { locationEngine.addLocationEngineListener(this); }
+    }
+
+    @SuppressWarnings("MissingPermission")
+    private void initializeLocationLayer() {
+        if (mapView == null) { Log.d(tag, "mapView is null"); }
+        else {
+            if (map == null) { Log.d(tag, "map is null"); }
+            else {
+                locationLayerPlugin = new LocationLayerPlugin(mapView, map, locationEngine);
+                locationLayerPlugin.setLocationLayerEnabled(true);
+                locationLayerPlugin.setCameraMode(CameraMode.TRACKING);
+                locationLayerPlugin.setRenderMode(RenderMode.NORMAL);
+            }
+        }
+    }
+
+    private void setCameraPosition(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        map.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if (location == null) { Log.d(tag, "[onLocationChanged] location is null"); }
+        else {
+            Log.d(tag, "[onLocationChanged] location is not null");
+            originLocation = location;
+            setCameraPosition(location);
+        }
+    }
+
+    @Override
+    @SuppressWarnings("MissingPermission")
+    public void onConnected() {
+        Log.d(tag, "[onConnected] requesting location updates");
+        locationEngine.requestLocationUpdates();
+    }
+
+    @Override
+    public void onExplanationNeeded(List<String> permissionsToExplain) { Log.d(tag, "Permissions: " + permissionsToExplain.toString()); }
+
+    @Override
+    public void onPermissionResult(boolean granted) {
+        Log.d(tag, "[onPermissionResult] granted == " + granted);
+        if (granted) { enableLocation(); }
+        else {
+        // Open a dialogue with the user
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_basic, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
